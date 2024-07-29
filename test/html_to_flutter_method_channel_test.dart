dc59bae7fa55b58297b7e66a340760e3571a21de
devaryakjha <dev.jha.arya@gmail.com>
2024-07-29 15:40:01 +0530

    refactor: Remove unused files and dependencies

--- test/html_to_flutter_method_channel_test.dart
+++ test/html_to_flutter_method_channel_test.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:html_to_flutter/html_to_flutter_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:html_to_flutter/html_to_flutter_method_channel.dart';
 
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+// void main() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelHtmlToFlutter platform = MethodChannelHtmlToFlutter();
-  const MethodChannel channel = MethodChannel('html_to_flutter');
+  
+//   const MethodChannel channel = MethodChannel('html_to_flutter');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
+//   setUp(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+//       channel,
+//       (MethodCall methodCall) async {
+//         return '42';
+//       },
+//     );
+//   });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
+//   tearDown(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await platform.getPlatformVersion(), '42');
+//   });
+// }
--- test/html_to_flutter_test.dart
+++ test/html_to_flutter_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:html_to_flutter/html_to_flutter_platform_interface.dart';
-import 'package:html_to_flutter/html_to_flutter_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:html_to_flutter/html_to_flutter_platform_interface.dart';
+// import 'package:html_to_flutter/html_to_flutter_method_channel.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockHtmlToFlutterPlatform
-    with MockPlatformInterfaceMixin
-    implements HtmlToFlutterPlatform {
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+// class MockHtmlToFlutterPlatform
+//     with MockPlatformInterfaceMixin
+//     implements HtmlToFlutterPlatform {
+//   @override
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
-void main() {
-  final HtmlToFlutterPlatform initialPlatform = HtmlToFlutterPlatform.instance;
+// void main() {
+//   final HtmlToFlutterPlatform initialPlatform = HtmlToFlutterPlatform.instance;
 
-  test('$MethodChannelHtmlToFlutter is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelHtmlToFlutter>());
-  });
+//   test('$MethodChannelHtmlToFlutter is the default instance', () {
+//     expect(initialPlatform, isInstanceOf<MethodChannelHtmlToFlutter>());
+//   });
 
-  test('getPlatformVersion', () async {
-    MockHtmlToFlutterPlatform fakePlatform = MockHtmlToFlutterPlatform();
-    HtmlToFlutterPlatform.instance = fakePlatform;
-  });
-}
+//   test('getPlatformVersion', () async {
+//     MockHtmlToFlutterPlatform fakePlatform = MockHtmlToFlutterPlatform();
+//     HtmlToFlutterPlatform.instance = fakePlatform;
+//   });
+// }
