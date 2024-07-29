f299d54c3a0f8cb4cb14a2986619b525d5a85d7f
devaryakjha <dev.jha.arya@gmail.com>
2024-07-29 15:27:59 +0530

    chore: Update HtmlShowcase page with extended app bar and button navigation

--- example/lib/pages/showcase.dart
+++ example/lib/pages/showcase.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HtmlShowcase extends StatelessWidget {
   const HtmlShowcase({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text('Html to Flutter Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              onPressed: () {
+                context.pushNamed('/basic-example');
+              },
+              child: const Text('Basic Example'),
+            ),
+            const SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {
+                context.pushNamed('/advanced-example');
+              },
+              child: const Text('Advanced Example'),
+            ),
+            const SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {
+                context.pushNamed('/blog-post-example');
+              },
+              child: const Text('Blog Post Example'),
+            ),
+            const SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {
+                context.pushNamed('/custom-example');
+              },
+              child: const Text('Custom Example'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
