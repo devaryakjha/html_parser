2b241af85e3df41f67489364970ef4d6e3b68c89
devaryakjha <dev.jha.arya@gmail.com>
2024-07-31 01:40:40 +0530

    refactor: Remove deprecated textScaleFactor usage in TextHtmlWidget

--- lib/src/core/widgets/text.dart
+++ lib/src/core/widgets/text.dart
@@ -12,12 +12,6 @@ final class TextHtmlWidget extends Text implements IHtmlWidget {
     super.locale,
     super.softWrap,
     super.overflow,
-    @Deprecated(
-      'Use textScaler instead. '
-      'Use of textScaleFactor was deprecated in preparation for the upcoming nonlinear text scaling support. '
-      'This feature was deprecated after v3.12.0-2.0.pre.',
-    )
-    super.textScaleFactor,
     super.textScaler,
     super.maxLines,
     super.semanticsLabel,
