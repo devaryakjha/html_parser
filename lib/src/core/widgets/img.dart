a6c63151234135af43c51fbbcb922135acb07f65
devaryakjha <dev.jha.arya@gmail.com>
2024-08-01 02:06:02 +0530

    refactor: Update ImageHtmlWidget to use FullScreenWidget for child display

--- lib/src/core/widgets/img.dart
+++ lib/src/core/widgets/img.dart
@@ -58,7 +58,7 @@ final class ImageHtmlWidget extends StatelessWidget implements IHtmlWidget {
             duration: const Duration(milliseconds: 500),
             curve: Curves.easeOut,
             opacity: frame == null ? 0 : 1,
-            child: child,
+            child: FullScreenWidget(child: child),
           ),
         );
       },
