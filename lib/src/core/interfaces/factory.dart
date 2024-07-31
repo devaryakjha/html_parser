b5ed63b4f24a31bc5526cf3fff05e53cd80a0c43
devaryakjha <dev.jha.arya@gmail.com>
2024-07-31 15:19:20 +0530

    refactor: Update IHtmlWidgetFactory to extend Equatable

--- lib/src/core/interfaces/factory.dart
+++ lib/src/core/interfaces/factory.dart
@@ -1,8 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:html_to_flutter/html_to_flutter.dart' show IHtmlWidget;
 
 /// An interface for creating instances of [IHtmlWidget].
-abstract interface class IHtmlWidgetFactory<Widget extends IHtmlWidget> {
+abstract interface class IHtmlWidgetFactory<Widget extends IHtmlWidget>
+    extends Equatable {
   /// Creates a new instance of [IHtmlWidgetFactory].
   const IHtmlWidgetFactory(this.widget);
 
@@ -12,4 +14,7 @@ abstract interface class IHtmlWidgetFactory<Widget extends IHtmlWidget> {
   /// Creates a new instance of [IHtmlWidgetFactory] from the given [node].
   const IHtmlWidgetFactory.fromNode(final dom.Node node)
       : widget = const IHtmlWidget.placeholder() as Widget;
+
+  @override
+  List<Object?> get props => [widget];
 }
