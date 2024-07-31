7e938453f628ccf57337e614c67e6ffccc266f06
devaryakjha <dev.jha.arya@gmail.com>
2024-08-01 02:48:29 +0530

    refactor: Update ContainerHtmlWidgetFactory to handle div elements

--- lib/src/core/factories/container.dart
+++ lib/src/core/factories/container.dart
@@ -8,14 +8,32 @@ final class ContainerHtmlWidgetFactory
     implements IHtmlWidgetFactory<ContainerHtmlWidget> {
   const ContainerHtmlWidgetFactory(this._builder);
 
+  static ContainerType _createContainerType(final dom.Node node) {
+    final classses = node.attributes['class'];
+    final styles = node.attributes['style'];
+
+    return classses?.contains('row') ??
+            classses?.contains('flex-row') ??
+            classses?.contains('flex-row-reverse') ??
+            styles?.contains('display: flex;') ??
+            styles?.contains('flex-direction: row;') ??
+            styles?.contains('flex-direction: row-reverse;') ??
+            false
+        ? ContainerType.row
+        : ContainerType.column;
+  }
+
   factory ContainerHtmlWidgetFactory.fromNode(
     final dom.Node node,
     final UnsupportedParser unsupportedParser,
   ) {
     final children = node.nodes.map(unsupportedParser).whereNotNull().toList();
     return ContainerHtmlWidgetFactory((context) {
+      final type = _createContainerType(node);
+
       return ContainerHtmlWidget(
         children: children.map((e) => e.builder).toList(),
+        type: type,
       );
     });
   }
