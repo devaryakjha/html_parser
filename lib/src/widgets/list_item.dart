2198542b391f9c0fef90ae6bac15c6e2bbebf510
devaryakjha <dev.jha.arya@gmail.com>
2024-08-01 16:48:30 +0530

    chore: Refactor ListItemHtmlWidget to improve code readability and maintainability

--- lib/src/widgets/list_item.dart
+++ lib/src/widgets/list_item.dart
@@ -2,15 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:html_to_flutter/html_to_flutter.dart';
 
+/// A widget that is used to render a list item.
+///
+/// represents `<li>` tag.
 final class ListItemHtmlWidget extends StatelessWidget {
-
+  /// Creates a new instance of [ListItemHtmlWidget].
   const ListItemHtmlWidget({
-    required this.title, required this.index, required this.source, super.key,
+    required this.title,
+    required this.index,
+    required this.source,
+    super.key,
     this.isOrdered = false,
   });
+
+  /// The index of the list item.
   final int index;
+
+  /// The title of the list item.
   final String title;
+
+  /// Whether the list is ordered.
   final bool isOrdered;
+
+  /// The source of the list item.
   final dom.Node source;
 
   @override
