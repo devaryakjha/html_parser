df65dc7dccea31dc761f6e65b5ce5be8af586fb9
devaryakjha <dev.jha.arya@gmail.com>
2024-08-01 16:46:50 +0530

    docs: Add factory documentation for `IHtmlParser` class

--- lib/src/interfaces/parser.dart
+++ lib/src/interfaces/parser.dart
@@ -4,10 +4,12 @@ import 'package:html_to_flutter/html_to_flutter.dart'
 
 /// An interface for parsing HTML strings.
 abstract interface class IHtmlParser with EquatableMixin {
-
+  /// Creates a new instance of [IHtmlParser].
   const IHtmlParser({required this.config});
+
+  /// The configuration for the parser.
   final HtmlConfig config;
 
   /// Parses the given [html] string and returns a list of [IHtmlWidget]s.
-  List<IHtmlWidgetFactory> parse(final String html);
+  List<IHtmlWidgetFactory> parse(String html);
 }
