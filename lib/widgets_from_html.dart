91f92fda0d252eb7baa93d3cb4c966e00fb4db17
devaryakjha <dev.jha.arya@gmail.com>
2024-08-07 17:30:53 +0530

    wip

--- lib/widgets_from_html.dart
+++ lib/widgets_from_html.dart
@@ -7,13 +7,21 @@ export 'src/interfaces/interfaces.dart';
 export 'src/widgets/widgets.dart';
 
 /// Represents a HTML Node.
+///
+/// redirects to [Node] from `package:html/dom.dart`.
 typedef HtmlNode = Node;
 
 /// Represents a HTML Element.
+///
+/// redirects to [Element] from `package:html/dom.dart`.
 typedef HtmlElement = Element;
 
 /// Represents a HTML Text Node.
+///
+/// redirects to [Text] from `package:html/dom.dart`.
 typedef HtmlText = Text;
 
 /// Represents a HTML Document.
+///
+/// redirects to [Document] from `package:html/dom.dart`.
 typedef HtmlDocument = Document;
