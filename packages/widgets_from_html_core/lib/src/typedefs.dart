f8ceda9e117510e62ed7cb957495f6429e61deab
devaryakjha <dev.jha.arya@gmail.com>
2024-08-08 01:50:18 +0530

    feat: Add typedefs.dart file for defining type aliases and function signatures

---
+++ packages/widgets_from_html_core/lib/src/typedefs.dart
@@ -0,0 +1,8 @@
+import 'package:widgets_from_html_core/widgets_from_html_core.dart';
+
+/// Signature for tap event handler.
+typedef OnLinkTap = void Function(
+  String? url, [
+  Map<String, String>? attributes,
+  Element? element,
+]);
