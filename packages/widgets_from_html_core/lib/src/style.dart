662ba0b032e848c18e87aafc475386c06b0d4d91
devaryakjha <dev.jha.arya@gmail.com>
2024-08-08 01:50:13 +0530

    feat: Add Style class for representing CSS properties

---
+++ packages/widgets_from_html_core/lib/src/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+/// {@template style}
+/// This class represents all supported CSS properties by this package.
+/// {@endtemplate}
+@immutable
+final class Style {
+  /// Creates a [Style].
+  ///
+  /// {@macro style}
+  const Style();
+}
